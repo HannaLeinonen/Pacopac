2c0ded9262f1440364098993e78eb6a5264a4e41
Theo Archer <thste001@gmail.comclear>
2024-02-29 15:14:00 +0100

    Home page done, cart component changed to dropdown function not yet complete for shoppingcart

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -2,10 +2,10 @@
   <div class="dropdown">
     <!-- OnClick show DropDownMenu -->
     <!-- <HamburgerMenu  class="dropdown-toggle" @click="toggleDropdown"/> -->
-<!-- Show HamburgerMenu always -->
-    <HamburgerMenu v-show="!isOpen" class="dropdown-toggle" @click="toggleDropdown"/>
+    <!-- Show HamburgerMenu always -->
+    <HamburgerMenuIcon v-show="!isOpen" class="dropdown-toggle" @click="toggleDropdown" />
     <!-- Show CloseMenu when isOpen is true -->
-    <CloseMenu v-show="isOpen" class="dropdown-toggle" @click="toggleDropdown"/>
+    <CloseMenu v-show="isOpen" class="dropdown-toggle" @click="toggleDropdown" />
 
     <!-- <button class="dropdown-toggle" @click="toggleDropdown">
       <span>&#9776;</span>
@@ -19,31 +19,31 @@
       </div>
 
       <div class="products">
-        <a href="#">
+        <a href="#" id="home">
           <div class="product-image">HOME</div>
         </a>
 
-        <a href="#">
-          <div class="product-image">HELLY HANSEN</div>
+        <a href="#" id="helly">
+          <div class="product-image">HELLY <br />HANSEN</div>
         </a>
 
-        <a href="#">
+        <a href="#" id="douche">
           <div class="product-image">DOUCHEBAGS</div>
         </a>
 
-        <a href="#">
+        <a href="#" id="nike">
           <div class="product-image">NIKE</div>
         </a>
 
-        <a href="#">
+        <a href="#" id="adidas">
           <div class="product-image">ADIDAS</div>
         </a>
 
-        <a href="#">
+        <a href="#" id="fjell">
           <div class="product-image">FJELLREVEN</div>
         </a>
       </div>
-      <a href="#">
+      <a href="#" id="sale">
         <div class="sale">
           <div>SALE</div>
         </div>
@@ -53,8 +53,8 @@
 </template>
 
 <script setup>
-import HamburgerMenu from './icons/HamburgerMenuIcon.vue';
-import CloseMenu from './icons/CloseMenuIcon.vue';
+import HamburgerMenuIcon from './icons/HamburgerMenuIcon.vue'
+import CloseMenu from './icons/CloseMenuIcon.vue'
 import { ref } from 'vue'
 
 const isOpen = ref(false)
@@ -65,29 +65,26 @@ const toggleDropdown = () => {
 </script>
 
 <style scoped>
-
 .dropdown-content {
   display: none;
-  position: fixed;
+  position: absolute;
   top: 100px;
   bottom: 0;
   right: -100%;
-  transition: right 0.6s;
+  transition: ease-in-out right 0.6s;
   background-color: #ffefe0;
   width: 390px;
   height: 531px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1000;
+  border-radius: 10px;
+  overflow-y: auto;
 }
 .dropdown-content.open {
   right: 0;
 }
 .dropdown-toggle {
   z-index: 2000;
-  transition: ease 0.7s;
-}
-.dropdown-toggle:hover {
-  color: #d9a87e;
 }
 
 .dropdown-content {
@@ -104,17 +101,18 @@ const toggleDropdown = () => {
 button {
   background-color: #2b3d2e;
   color: #fff7f1;
-  font-size: 14px;
+  font-size: 13px;
   font-weight: bold;
   border: none;
   height: 31px;
   width: 114px;
   transition: ease 0.5s;
+  border-radius: 10px;
+  border: 2px solid black;
 }
 
 #size-button:hover {
-  background-color: #35583b;
-  color: #d9a87e;
+  background-color: #222e24;
 }
 
 .products {
@@ -127,11 +125,11 @@ button {
   margin: 0 auto;
   max-width: 390px;
   font-size: 24px;
+  letter-spacing: 1px;
 }
 
 .sale {
-  background-color: #ff0000;
-  color: #fff7f1;
+  color: #ff0000;
   text-align: center;
   display: flex;
   align-items: center;
@@ -139,18 +137,110 @@ button {
   height: 99px;
   width: 370px;
   margin: 10px;
-  font-size: 24px;
+  font-size: 35px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: ease-in-out 0.7s;
+  background-position: bottom left;
+  border: 2px solid black;
+  background-image: url('/backpackImg/nikeLarge.webp');
+  background-position: center;
+  background-size: 50%;
+  letter-spacing: 2px;
 }
-
+.sale:hover {
+  background-position: top left;
+  font-size: 50px;
+}
 .product-image {
-  background-color: #d9a87e;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 99px;
   margin: 0;
   padding: 0;
+  transition: ease-in-out 0.5s;
+  border: black 2px solid;
+  border-radius: 10px;
 }
+.product-image:hover {
+  color: #d9a87e;
+  font-size: 35px;
+}
+#home {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('../imgs/home.webp');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 10px;
+  background-position: center;
+  transition: ease-in-out 0.7s;
+  background-position: right left;
+}
+#home:hover {
+  background-position: top left;
+}
+#nike {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/nikeLarge.webp');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 10px;
+  transition: ease-in-out 0.7s;
+  background-position: top left;
+}
+#nike:hover {
+  background-position: bottom left;
+}
+#adidas {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/adidasMedium.webp');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 10px;
+  transition: ease-in-out 0.7s;
+  background-position: top left;
+}
+#adidas:hover {
+  background-position: bottom left;
+}
+#douche {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/dbMedium.webp');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 10px;
+  transition: ease-in-out 0.7s;
+  background-position: top left;
+}
+#douche:hover {
+  background-position: bottom left;
+}
+#helly {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/hhXL.webp');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 10px;
+  transition: ease-in-out 0.7s;
+  background-position: top left;
+}
+#helly:hover {
+  background-position: bottom left;
+}
+#fjell {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/fjallravenLarge.webp');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 10px;
+  transition: ease-in-out 0.7s;
+  background-position: top left;
+}
+#fjell:hover {
+  background-position: bottom left;
+}
+
 a {
   -webkit-tap-highlight-color: transparent;
   text-decoration: none;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,193 @@
 <template>
-
-  <main></main>
-
+  <!-- BRANDBANNER HERE -->
+  <main>
+    <div class="new-arrivals"><h1>NEW ARRIVALS</h1></div>
+    <div class="sale-banner">
+      <h1 id="sale">SALE</h1>
+      <h1 id="sale">SALE</h1>
+      <h1 id="sale">SALE</h1>
+    </div>
+    <div class="hero-products">
+      <div id="slideshow">
+        <div
+          v-for="(slide, index) in slides"
+          :key="index"
+          class="slide"
+          :class="{ active: index === currentSlide }"
+          :style="{
+            backgroundImage:
+              'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(' + slide.image + ')'
+          }"
+        >
+          <h1>{{ slide.title }}</h1>
+          <!-- <img :src="slide.image" :alt="'Slide ' + (index + 1)" /> -->
+        </div>
+      </div>
+    </div>
+    <div class="sale-banner" id="sale2">
+      <h1 id="sale">SALE</h1>
+      <h1 id="sale">SALE</h1>
+      <h1 id="sale">SALE</h1>
+    </div>
+    <div class="inspo-div">
+      <h1>
+        "Fuel your wanderlust <br />
+        with our stylish and durable backpacks. <br />
+        Where will you go next?"
+      </h1>
+    </div>
+  </main>
 </template>
+<script setup>
+import { ref, computed, onMounted, onUnmounted } from 'vue'
+
+const slides = ref([
+  { image: 'imgs/urban.webp', title: 'Urban' },
+  { image: 'imgs/nature.webp', title: 'Nature' }
+])
+
+const currentSlide = ref(0)
+
+function nextSlide() {
+  currentSlide.value = (currentSlide.value + 1) % slides.value.length
+}
+
+onMounted(() => {
+  const interval = setInterval(nextSlide, 5000)
+  onUnmounted(() => {
+    clearInterval(interval)
+  })
+})
+</script>
+<style scoped>
+h1 {
+  letter-spacing: 2px;
+}
+#sale {
+  margin: 100px;
+  transition: ease, 0.7;
+  font-size: 80px;
+}
+
+.new-arrivals {
+  color: white;
+  display: flex;
+  height: 282px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url('../imgs/new-arrivals3.webp');
+  background-size: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.sale-banner {
+  color: #ff0000;
+  display: flex;
+  height: 154px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/nikeLarge.webp');
+  background-position: bottom left;
+  background-position: center;
+  background-size: contain;
+  transition: ease-in-out 1s;
+}
+.sale-banner:hover {
+  background-position: top left;
+}
+#sale2 {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('/backpackImg/fjallravenLarge.webp');
+}
+/* SlideShow */
+.hero-products {
+  color: white;
+  font-size: 30px;
+  height: 207px;
+  width: 100%;
+  text-align: center;
+}
+
+#slideshow {
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+}
+
+.slide {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transition: opacity 1s ease-in-out;
+  opacity: 0;
+}
+
+.slide.active {
+  opacity: 1;
+}
+.slide img {
+  width: 100%;
+  height: 207px;
+  object-fit: cover;
+}
+.slide h1 {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+}
+/* End of Slide */
+.inspo-div {
+  position: relative;
+  height: 400px;
+  color: white;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  overflow: hidden;
+}
+
+.inspo-div::before,
+.inspo-div::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  transition: opacity 3s ease;
+}
+
+.inspo-div::before {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('../imgs/inspo.webp');
+  opacity: 1;
+}
+
+.inspo-div::after {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('../imgs/winter-inspo.webp');
+  opacity: 0;
+}
+
+.inspo-div:hover::after {
+  opacity: 1;
+}
+
+.inspo-div:hover::before {
+  opacity: 0;
+}
+.inspo-div h1 {
+  position: relative;
+  z-index: 2;
+  color: white;
+  margin: 0;
+}
+</style>
