e637a023c70b745b463be3f7140b7a07e33e58cf
Theo Archer <thste001@gmail.comclear>
2024-03-01 10:52:05 +0100

    OVERFLOW !

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -33,6 +33,9 @@ import ShoppingCartComponent from './ShoppingCartComponent.vue'
 /* Importing the font KOULEN for the brandName */
 @import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
 
+* {
+  overflow-x: visible;
+}
 .navBar {
   display: flex;
   margin: 0 0 auto;
