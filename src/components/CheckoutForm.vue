9fc446e9350c5598e7ab2a047cdee8309ee03e08
HannaLeinonen <120415040+HannaLeinonen@users.noreply.github.com>
2024-03-12 22:05:18 +0100

    Ticket #6 done

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -1,30 +1,95 @@
 <template>
   <div class="formWrapper">
-    <form>
+    <form @submit.prevent="proceedToPayment"> <!-- On submit and valid form inputs, call proceedToPayment function -->
       <div>
-        <label for="firstName">First name</label>
-        <input required type="text" id="firstName" name="firstName" enterkeyhint="next" />
+        <label for="firstName">First name*</label>
+        <input
+          required
+          v-model="formData.firstName"
+          type="text"
+          id="firstName"
+          name="firstName"
+          enterkeyhint="next"
+        />
       </div>
       <div>
-        <label for="lastName">Last name</label>
-        <input required type="text" id="lastName" name="lastName" enterkeyhint="next" />
+        <label for="lastName">Last name*</label>
+        <input
+          required
+          v-model="formData.lastName"
+          type="text"
+          id="lastName"
+          name="lastName"
+          enterkeyhint="next"
+        />
       </div>
       <div>
-        <label for="email">Email</label>
-        <input required type="email" id="email" name="email" enterkeyhint="next" />
+        <label for="email">Email*</label>
+        <input
+          required
+          v-model="formData.email"
+          type="email"
+          id="email"
+          name="email"
+          enterkeyhint="next"
+        />
       </div>
       <div>
-        <label for="phoneNumber">Phone number</label>
-        <input required type="tel" id="phoneNumber" name="phoneNumber" enterkeyhint="next" />
+        <label for="phoneNumber">Phone number*</label>
+        <input
+          required
+          v-model="formData.phoneNumber"
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          enterkeyhint="next"
+        />
       </div>
       <div>
-        <label for="address">Delivery address</label>
-        <input required type="text" id="address" name="address" enterkeyhint="next" />
+        <label for="address">Delivery address*</label>
+        <input
+          required
+          v-model="formData.address"
+          type="text"
+          id="address"
+          name="address"
+          enterkeyhint="next"
+        />
       </div>
-      <button>PROCEED TO PAYMENT</button>
+      <button type="submit">PROCEED TO PAYMENT</button>
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        address: ''
+      }
+    }
+  },
+  methods: {
+    // If form is valid, send emit to parent "PaymentView" 
+    proceedToPayment() {
+      if (this.isFormValid()) {
+        this.$emit('goToPayment')
+      }
+    },
+    // Controlling if the form is correctly filled according to "required" attribut on inputs
+    isFormValid() {
+      const { firstName, lastName, email, phoneNumber, address } = this.formData
+
+      return firstName && lastName && email && phoneNumber && address
+    }
+  }
+}
+</script>
 
 <style scoped>
 .formWrapper {
@@ -43,10 +108,16 @@ button {
   cursor: pointer;
 }
 input {
-  border: 1px solid lightgray;
+  border: 2px solid lightgray;
   width: 100%;
   height: 1.7rem;
   border-radius: 5px;
+  -webkit-transition: 0.7s;
+  transition: 0.7s;
+  outline: none;
+}
+input:focus {
+  border: 2px solid gray;
 }
 label {
   display: block;
