79399dda2928dd1b84336934cc9843aeaf5f87eb
HannaLeinonen <120415040+HannaLeinonen@users.noreply.github.com>
2024-03-13 19:15:14 +0100

    Some styling fixed in PaymentView, CartItems and CheckoutForm. Fixed item price in Cart

--- src/components/ShoppingCartComponent.vue
+++ src/components/ShoppingCartComponent.vue
@@ -31,7 +31,7 @@
 
               </button>
 
-              <h3>${{ item.price }}</h3>
+              <h3>${{ item.price * item.quantity }}</h3>
             </div>
           </div>
         </div>
@@ -62,7 +62,6 @@ const cartItems = computed(() => cartStore.cartItems)
 const removeItem = cartStore.removeItem
 const incrementItemQuantity = cartStore.incrementItemQuantity
 const decrementItemQuantity = cartStore.decrementItemQuantity
-
 const totalCost = computed(() => cartStore.totalCost)
 const toggleDropdown = () => {
   isOpen.value = !isOpen.value
@@ -72,7 +71,10 @@ function goToCheckout() {
     router.push('/checkout')
     isOpen.value = false
 }
+
+
 </script>
+
 
 <style scoped>
 .dropdown-content {
